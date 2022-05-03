a8c2893ce324ae2e9da0d8ce305b43317ab18156
HEADHUNTER9509 <104822051+HEADHUNTER9509@users.noreply.github.com>
2022-05-03 13:00:46 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+print("Simple Calculator") 
+x = 10 #Integer data type can be changed
+y = 20 #Integer data type can be changed
+
+sum = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+remainder = x % y
+
+print("The sum of x and y is ", sum)
+print("The difference between x and y is ", difference)
+print("The product of x and y is ",product)
+print("The quotient of x by y is ", quotient)
+print("The remainder of x by y is ", remainder)
